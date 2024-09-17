bbc34975e3cc877b0174ad3973bed8862789ede6
Md Al Amin <al.amin@bizzntek.com>
2024-09-17 23:16:25 +0600

    gamme state , last state and random play and reset are added

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,7 @@ function Header() {
   return (
     <div className="pt-2 pl-2" style={{ borderBottom: "1px solid #777" }}>
       <img src={logo} style={{ height: "35px", verticalAlign: "top" }}></img>
-      <span className="h2 pt-4 m-2 text-white-50">
-        React Project - CountOPedia
-      </span>
+      <span className="h2 pt-4 m-2 text-white-50">CountOPedia</span>
     </div>
   );
 }
